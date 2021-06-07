b49617adb8229983d30a7643abfd00cd202ba498
igrphillipe <igrphillipe@gmail.com>
2021-06-07 20:24:51 -0300

    create controllers dir

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,7 @@
 const path = require('path');
-const keystone = require('keystone');
 const cors = require('cors');
 
-const Post = keystone.list('Posts');
+const postController = require('../controllers/postController');
 
 module.exports = (app) => {
   app.use(cors());
@@ -11,17 +10,9 @@ module.exports = (app) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 
-  app.get('/api/posts', (req, res) => {
-    Post.model.find((err, data) => {
-      if (err) {
-        res.status(500).send('DB Error');
-      } else {
-        res.send(data);
-      }
-    });
-  });
+  app.get('/api/posts', postController.getPostController);
 
   app.get('*', (req, res) => {
-		res.redirect('/');
-	});
+    res.redirect('/');
+  });
 };
