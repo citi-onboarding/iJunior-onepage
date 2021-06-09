957fd66021da25d787fdaa15dc9cc5c7224e3067
Mauricio Guimarães <mfgf@cin.ufpe.br>
2021-06-09 18:49:27 -0300

    Feature(backend-developtment-section): develop the development section of the back-end.

---
+++ server/controllers/developmentController.js
@@ -0,0 +1,17 @@
+const keystone = require('keystone');
+
+const Development = keystone.list('Development');
+
+module.exports = {
+  getPostController(req, res) {
+    Development.model.find((err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('DB Error');
+      } else {
+        res.send(data);
+      }
+    });
+  }
+
+}
