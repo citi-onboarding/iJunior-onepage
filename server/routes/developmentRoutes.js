b9057b2ccea7749023b64e16d9bc39fd4167c77e
Mauricio Guimarães <mfgf@cin.ufpe.br>
2021-06-10 19:20:27 -0300

    Bugfix(server-setRoute): create a single file for routes

--- server/routes/developmentRoutes.js
+++
@@ -1,5 +0,0 @@
-const developmentController = require('../controllers/developmentController');
-
-module.exports = (app) => {
-  app.get('/api/development', developmentController.getDevelopmentController);
-};
