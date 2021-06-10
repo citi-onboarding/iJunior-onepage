b5d30d2c010ef84df4d771ef392e9d128cb8ac2b
Mauricio Guimarães <52111341+MauricioFGF@users.noreply.github.com>
2021-06-10 12:20:16 -0300

    Bugfix(backend-development-section): name correction

--- server/routes/developmentRoutes.js
+++ server/routes/developmentRoutes.js
@@ -1,5 +1,5 @@
 const developmentController = require('../controllers/developmentController');
 
 module.exports = (app) => {
-  app.get('/api/development', developmentController.getPostController);
+  app.get('/api/development', developmentController.getDevelopmentController);
 };
