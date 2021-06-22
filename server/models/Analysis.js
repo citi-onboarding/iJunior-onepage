76c84ced071803c1e043b9172e18df4a607d7e1c
Carolina Berrafato <carolberrafato13@gmail.com>
2021-06-22 16:34:26 -0300

    bugfix(analysis-backend): fixed the texts backend

--- server/models/Analysis.js
+++ server/models/Analysis.js
@@ -9,13 +9,45 @@ Analysis.add({
             type: Types.Text,
             required: true,
             initial: true,
-            label: "Descrição no primeiro "
+            max: 30,
+            label: "Descrição do tópico no primeiro texto"
         },
         secondTopic: {
             type: Types.Text,
             required: true,
             initial: true,
-            label: "Descrição no segundo parágrafo"
+            max: 30,
+            label: "Descrição do tópico no primeiro texto"
+        },
+        thirdTopic: {
+            type: Types.Text,
+            required: true,
+            initial: true,
+            max: 30,
+            label: "Descrição do tópico no primeiro texto"
+        },
+    },
+    secondText: {
+        firstTopic: {
+            type: Types.Text,
+            required: true,
+            initial: true,
+            max: 30,
+            label: "Descrição do tópico no segundo texto"
+        },
+        secondTopic: {
+            type: Types.Text,
+            required: true,
+            initial: true,
+            max: 30,
+            label: "Descrição do tópico no segundo texto"
+        },
+        thirdTopic: {
+            type: Types.Text,
+            required: true,
+            initial: true,
+            max: 30,
+            label: "Descrição do tópico no segundo texto"
         },
     },
     firstImage: {
