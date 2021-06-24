05c742c9cd507352c581e56ee26d62405e316569
Mauricio Guimarães <52111341+MauricioFGF@users.noreply.github.com>
2021-06-24 17:09:49 -0300

    Update Footer.js

--- client/src/views/Footer/Footer.js
+++ client/src/views/Footer/Footer.js
@@ -21,7 +21,7 @@ const Footer = () => {
                         <img src={Seta} alt="seta para cima"/>
                         <p>Volta para o topo</p>
                     </div>
-                    <p className="footer-button-p" >Made with ❤ and &lt;/&gt; by CITI</p>
+                    <p className="footer-button-p" >Made with &lt;/&gt; and ❤ by CITI</p>
                 </div>
             </div>
         </div>
